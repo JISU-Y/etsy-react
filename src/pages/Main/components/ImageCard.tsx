58c347f99501a6a0908073f7b59d9b485f2215e4
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-07 11:30:50 +0900

    Feat: ImageCard 컴포넌트 생성 ImageCard 컴포넌트 생성 및 Hover interaction 추가

---
+++ src/pages/Main/components/ImageCard.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components';
+import Favorite from '../../../components/Favorite';
+import { currency } from '../../../constants/localeSetting';
+
+interface ImageCardProps {
+  image: string;
+  width: number;
+  height: number;
+  hasPriceTag: boolean;
+  price: string;
+}
+
+const ImageCard = ({ ...props }: ImageCardProps) => {
+  const { image, hasPriceTag, price } = props;
+
+  return (
+    <ImgContainer {...props}>
+      <img src={image} alt="item" />
+      <FavoriteWrap>
+        <Favorite size="small" />
+      </FavoriteWrap>
+      {hasPriceTag && (
+        <PriceTag>
+          {currency} {price}
+        </PriceTag>
+      )}
+    </ImgContainer>
+  );
+};
+
+const FavoriteWrap = styled.div`
+  position: absolute;
+  top: 6px;
+  right: 6px;
+  opacity: 0;
+  transform: translateY(10px);
+  transition: all 0.2s ease-in-out;
+`;
+
+const ImgContainer = styled.div<ImageCardProps>`
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: box-shadow 0.2s ease-in-out;
+  img {
+    width: ${props => props.width}px;
+    height: ${props => props.height}px;
+  }
+  &:hover {
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.2);
+  }
+  &:hover > ${FavoriteWrap} {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const PriceTag = styled.div`
+  position: absolute;
+  bottom: 8px;
+  left: 6px;
+  padding: 6px 9px;
+  background-color: white;
+  border-radius: 15px;
+  border: 1px solid #c4c4c4;
+  font-size: 13px;
+  font-weight: 500;
+`;
+export default ImageCard;
