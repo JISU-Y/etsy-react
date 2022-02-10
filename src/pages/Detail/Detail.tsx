eb21b67b34bac97a40e1b71fe754bf978abbf65d
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-10 13:36:48 +0900

    Refactor: Detail 페이지 작업 Swiper fade effect 적용, Thumbnail navigation 스타일 적용 ImageSection 컴포넌트 분리

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
-import SwiperCore from 'swiper';
-
-import { FreeMode, Navigation, Thumbs, EffectFade } from 'swiper';
 import { useLocation } from 'react-router-dom';
 import useSWR from 'swr';
 import { getProductDetails } from '../../utils/axios';
-import Favorite from '../../components/Favorite';
+import ImageSection from './components/ImageSection';
 
-interface DetailsProps {
+export interface DetailsProps {
   id: number;
   images: string[];
   title: string;
@@ -30,11 +22,9 @@ interface DetailsProps {
   quickDelivery: boolean;
 }
 
-// const settings_swiper
-
 export default function Detail() {
-  const [thumbsSwiper, setThumbsSwiper] = React.useState<SwiperCore>();
   const [images, setImages] = React.useState<string[]>([]);
+
   const {
     state: { id },
   } = useLocation<{ id: number }>();
@@ -42,7 +32,6 @@ export default function Detail() {
   const { data } = useSWR('productDetails.json', url => getProductDetails(url));
 
   React.useEffect(() => {
-    console.log(data?.data.data.filter((el: { id: number }) => el.id === id));
     setImages(
       data?.data.data.filter((el: { id: number }) => el.id === id)[0].images
     );
@@ -52,98 +41,9 @@ export default function Detail() {
 
   return (
     <Container>
-      <ImageContainer>
-        <IconWrap>
-          <Favorite size="large" />
-        </IconWrap>
-        <Swiper
-          onSwiper={setThumbsSwiper}
-          spaceBetween={10}
-          slidesPerView={images?.length + 1}
-          watchSlidesProgress={true}
-          modules={[Navigation, Thumbs]}
-          direction="vertical"
-          className="mySwiper"
-        >
-          {images?.map((imageUrl: string, index: number) => (
-            <SwiperSlide key={imageUrl}>
-              <img src={imageUrl} alt={`preview${index}`} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <Swiper
-          loop={true}
-          spaceBetween={5}
-          modules={[FreeMode, Navigation, Thumbs, EffectFade]}
-          navigation={true}
-          effect={'fade'}
-          fadeEffect={{ crossFade: true }}
-          speed={2000}
-          className="mySwiper2"
-        >
-          {images?.map((imageUrl: string, index: number) => (
-            <SwiperSlide key={imageUrl}>
-              <img src={imageUrl} alt={`preview${index}`} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </ImageContainer>
+      <ImageSection images={images} />
     </Container>
   );
 }
 
 const Container = styled.div``;
-
-const ImageContainer = styled.div`
-  display: flex;
-  width: 700px;
-  height: 540px;
-  position: relative;
-  .mySwiper2 {
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-    img {
-      width: 100%;
-      height: 100%;
-    }
-    .swiper-button-prev,
-    .swiper-button-next {
-      width: 48px;
-      height: 48px;
-      border-radius: 50%;
-      background-color: #fff;
-      color: black;
-    }
-    .swiper-button-prev::after,
-    .swiper-button-next::after {
-      font-size: 24px;
-    }
-  }
-  .mySwiper {
-    width: 10%;
-    height: 100%;
-    margin: 0 5px;
-    img {
-      width: 60px;
-      height: 60px;
-      border-radius: 10px;
-      margin: 5px 0;
-      cursor: pointer;
-    }
-    .swiper-wrapper {
-      height: auto;
-      flex-direction: column;
-    }
-    .swiper-slide {
-      height: 60px;
-    }
-  }
-`;
-
-const IconWrap = styled.div`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  z-index: 2;
-`;
---
+++ src/pages/Detail/components/ImageSection.tsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import 'swiper/css/effect-fade';
+import SwiperCore from 'swiper';
+
+import { FreeMode, Navigation, Thumbs, EffectFade } from 'swiper';
+import Favorite from '../../../components/Favorite';
+import { DetailsProps } from '../Detail';
+
+interface Props {
+  images: DetailsProps['images'];
+}
+
+const swiperSettingType = {
+  effectType: 'fade',
+  slidesPerView: 'auto',
+  direction: 'vertical',
+} as const;
+
+const ThumbSwiperSettings = {
+  spaceBetween: 10,
+  slidesPerView: swiperSettingType.slidesPerView,
+  watchSlidesProgress: true,
+  modules: [Navigation, Thumbs],
+  direction: swiperSettingType.direction,
+};
+
+const MainSwiperSettings = {
+  loop: true,
+  spaceBetween: 5,
+  modules: [FreeMode, Navigation, Thumbs, EffectFade],
+  navigation: true,
+  effect: swiperSettingType.effectType,
+  fadeEffect: { crossFade: true },
+  speed: 500,
+  watchSlidesProgress: true,
+  allowTouchMove: false,
+  lazy: true,
+};
+
+function ImageSection({ images }: Props) {
+  const [thumbsSwiper, setThumbsSwiper] = React.useState<SwiperCore>();
+
+  return (
+    <ImageContainer>
+      <IconWrap>
+        <Favorite size="large" />
+      </IconWrap>
+      <Swiper
+        {...ThumbSwiperSettings}
+        onSwiper={setThumbsSwiper}
+        className="mySwiper"
+      >
+        {images?.map((imageUrl: string, index: number) => (
+          <SwiperSlide key={imageUrl}>
+            <img src={imageUrl} alt={`preview${index}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Swiper
+        {...MainSwiperSettings}
+        thumbs={{
+          swiper: thumbsSwiper,
+        }}
+        className="mySwiper2"
+      >
+        {images?.map((imageUrl: string, index: number) => (
+          <SwiperSlide key={imageUrl}>
+            <img src={imageUrl} alt={`preview${index}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </ImageContainer>
+  );
+}
+
+const ImageContainer = styled.div`
+  display: flex;
+  width: 700px;
+  height: 540px;
+  position: relative;
+  .mySwiper2 {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .swiper-button-prev,
+    .swiper-button-next {
+      width: 48px;
+      height: 48px;
+      border-radius: 50%;
+      background-color: #fff;
+      color: black;
+    }
+    .swiper-button-prev::after,
+    .swiper-button-next::after {
+      font-size: 24px;
+    }
+  }
+  .mySwiper {
+    width: 10%;
+    height: 100%;
+    margin: 0 5px;
+    img {
+      width: 60px;
+      height: 60px;
+      border-radius: 10px;
+      cursor: pointer;
+    }
+    .swiper-wrapper {
+      height: auto;
+      flex-direction: column;
+    }
+    .swiper-slide {
+      height: 60px;
+    }
+    .swiper-slide {
+      opacity: 0.5;
+    }
+    .swiper-slide-thumb-active {
+      opacity: 1;
+      img {
+        border: 2px solid #000;
+      }
+    }
+  }
+`;
+
+const IconWrap = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 2;
+`;
+
+export default ImageSection;
